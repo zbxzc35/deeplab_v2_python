75143fe015e72d3eebdb8854e5cc585afbfdc9d1
wuhuikai <huikaiwu@icloud.com>
2016-12-07 23:31:38 -0500

    MODIFY: Add display param & mv out

---
+++ SolverCreator.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+def create_solver(proto_path, train_net, snapshot_prefix, display=20, snapshot=20000, power=0.9, base_lr=1e-3, momentum=0.5, weight_decay=0.0005):
+    with open('/home/wuhuikai/Segmentation/Deeplab_v2/exper/solver_template') as f:
+        proto = f.read()
+    proto = proto.format(train_net, power, base_lr, display, momentum, weight_decay, snapshot, snapshot_prefix)
+    with open(proto_path, 'w') as f:
+        f.write(proto)
--- voc12/deeplab_v2_vgg16/scripts/SolverCreator.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-def create_solver(proto_path, train_net, snapshot_prefix, snapshot=20000, power=0.9, base_lr=1e-3, momentum=0.9, weight_decay=0.0005):
-    with open('/home/wuhuikai/Segmentation/Deeplab_v2/exper/solver_template') as f:
-        proto = f.read()
-    proto = proto.format(train_net, power, base_lr, momentum, weight_decay, snapshot, snapshot_prefix)
-    with open(proto_path, 'w') as f:
-        f.write(proto)
