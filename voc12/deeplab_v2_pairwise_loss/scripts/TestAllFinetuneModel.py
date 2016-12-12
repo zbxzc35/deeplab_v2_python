0a29a83649237aa286c7a5ce7feb0b00f69af163
wuhuikai <huikaiwu@icloud.com>
2016-12-12 05:30:54 -0500

    EXP: Give up pairwise loss

--- voc12/deeplab_v2_pairwise_loss/scripts/TestAllFinetuneModel.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-path = '/home/wuhuikai/Segmentation/Deeplab_v2/exper/'
-for i in xrange(1000, 20001, 1000):
-	subprocess.call("python {}Test.py 0 deeplab_v2_pairwise_loss train_iter_{} fc8".format(path, i), shell=True)
-	subprocess.call("python {}PostNoneEval.py 16 deeplab_v2_pairwise_loss fc8 train".format(path), shell=True)
